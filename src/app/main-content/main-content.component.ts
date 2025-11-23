5a1b9e7fb66c889cba3a22e5e723ea1cf9e10204
milosdimi <132084913+milosdimi@users.noreply.github.com>
2025-11-23 17:17:02 +0100

    15 - Projekt: Struktur erweitern

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { landingPageComponent } from './landing-page/landingPage.component';
 import { OurMenuComponent } from './our-menu/our-menu.component';
+import { HowToOrderComponent } from './how-to-order/how-to-order.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [landingPageComponent, OurMenuComponent],
+  imports: [landingPageComponent, OurMenuComponent, HowToOrderComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
 })
