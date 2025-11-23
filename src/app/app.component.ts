e2cfc67261e3b8ca5bc8bae9878c92f5f1f2acf8
milosdimi <132084913+milosdimi@users.noreply.github.com>
2025-11-23 21:18:50 +0100

    imprint navbar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { MainContentComponent } from "./main-content/main-content.component";
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { FindUsComponent } from './main-content/find-us/find-us.component';
+import { NavbarComponent } from './main-content/landing-page/navbar/navbar.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainContentComponent, FooterComponent, ImprintComponent, FindUsComponent],
+  imports: [RouterOutlet, NavbarComponent, MainContentComponent, FooterComponent, ImprintComponent, FindUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
