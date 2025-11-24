b2be87a0a72cd3ba8cec998faa277262972fb9c6
milosdimi <132084913+milosdimi@users.noreply.github.com>
2025-11-24 23:35:41 +0100

    responsive updates fixed

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink], // ← DAS FEHLT BEI DIR!
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
